ef6a796dd3eda44c5577b80b2ad11ef8c749a474
Andrew Mackintosh <59948594+AndrewMackintosh91@users.noreply.github.com>
2024-03-29 07:03:37 +0000

    Update helloWorld.py
    Added a readout for today's date

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,5 @@
+from datetime import datetime, date
+
+today_date = date.today()
 user_name = input("Type your name: ")
-print(f"Hello {user_name}!")
+print(f"Hello {user_name}! Today is {today_date}!")
